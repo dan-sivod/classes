a8ae13079c75cfe04b0d775b237c4bb696ec0e43
Danila Sivodedov <dan-sivod@mail.ru>
2017-05-20 17:13:35 +0300

    Создана функция подключения файла

--- db.php
+++ db.php
@@ -2,5 +2,18 @@
 
 class Database
 {
-    
+    public static $db_connect;
+    public static $db_select;
+
+    public static function connection($host, $user, $password, $db_name)
+    {
+        self::$db_connect = mysqli_connect($host, $user, $password, $db_name);
+        if(!self::$db_connect){
+            echo 'Ошибка!' . PHP_EOL;
+            echo 'Код ошибки: ' . mysqli_connect_errno() . PHP_EOL;
+            echo 'Текст ошибки: ' . mysqli_connect_error() . PHP_EOL;
+            exit;
+        }
+        echo 'Соединение установлено'. PHP_EOL;
+    }
 }
