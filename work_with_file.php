5f5de3dbe3103c31808c15a1b96533034bd004a7
Danila Sivodedov <dan-sivod@mail.ru>
2017-05-29 23:31:49 +0300

    Начал писать класс для работы с файлами и каталогами

--- work_with_file.php
+++ work_with_file.php
@@ -1,7 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: oem
- * Date: 29.05.17
- * Time: 22:37
- */
+
+class Work_with_file
+{
+    static public function read_file_in_binary_mode($path)
+    {
+        $file_or_path = '"'."$path".'"';
+        $result = fopen($file_or_path,"rb") or die();
+        return $result;
+    }
+    static public function read_file_in_text_mode($path)
+    {
+        $file_or_path = '"'."$path".'"';
+        $result = fopen($file_or_path,"rt") or die();
+        return $result;
+    }
+    static public function write_file_in_binary_mode($path)
+    {
+
+    }
+    static public function write_file_in_text_mode($path)
+    {
+
+    }
+    static public function close_file($path)
+    {
+
+    }
+}
